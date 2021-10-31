9197ddbd7330aa8fdf5964454b3523b5fb311ecb
Ruslan Parchiev <ruinhorn@gmail.com>
2021-10-31 13:13:58 +0300

    - network

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textViewHello = findViewById<TextView>(R.id.tvHello)
         textViewHello.text = "Hello from code!"
-
-
     }
 }
