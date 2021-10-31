4ba41a19d77f776f4225aa1fba04c00051393ef8
Ruslan Parchiev <ruinhorn@gmail.com>
2021-10-31 14:29:31 +0300

    - выводим данные на экран колхозно

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -3,14 +3,39 @@ package com.example.weatherfetcher
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherfetcher.feature.weather_screen.WeatherInteractor
+import com.example.weatherfetcher.feature.weather_screen.data.WeatherApiClient
+import com.example.weatherfetcher.feature.weather_screen.data.WeatherRemoteSource
+import com.example.weatherfetcher.feature.weather_screen.data.WeatherRepoImpl
+import com.example.weatherfetcher.feature.weather_screen.ui.WeatherScreenPresenter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    private lateinit var presenter: WeatherScreenPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        presenter = WeatherScreenPresenter(
+            WeatherInteractor(
+                WeatherRepoImpl(
+                    WeatherRemoteSource(WeatherApiClient.getApi())
+                )
+            )
+        )
+
+        var weather = ""
         val textViewHello = findViewById<TextView>(R.id.tvHello)
-        textViewHello.text = "Hello from code!"
+
+        GlobalScope.launch {
+            withContext(Dispatchers.Main) {
+                textViewHello.text = presenter.getWeather()
+            }
+        }
     }
 }
