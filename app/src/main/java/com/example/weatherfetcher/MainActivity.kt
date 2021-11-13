25d691e57e5035fd2fe555726ea61434f6c40d28
Ruslan Parchiev <ruinhorn@gmail.com>
2021-11-13 13:56:46 +0300

    - начали делать архитектуру

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -3,38 +3,27 @@ package com.example.weatherfetcher
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.weatherfetcher.feature.weather_screen.WeatherInteractor
-import com.example.weatherfetcher.feature.weather_screen.data.WeatherApiClient
-import com.example.weatherfetcher.feature.weather_screen.data.WeatherRemoteSource
-import com.example.weatherfetcher.feature.weather_screen.data.WeatherRepoImpl
-import com.example.weatherfetcher.feature.weather_screen.ui.WeatherScreenPresenter
+import com.example.weatherfetcher.feature.weather_screen.ui.WeatherScreenViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var presenter: WeatherScreenPresenter
+    private val viewModel: WeatherScreenViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        presenter = WeatherScreenPresenter(
-            WeatherInteractor(
-                WeatherRepoImpl(
-                    WeatherRemoteSource(WeatherApiClient.getApi())
-                )
-            )
-        )
 
         var weather = ""
         val textViewHello = findViewById<TextView>(R.id.tvHello)
 
         GlobalScope.launch {
             withContext(Dispatchers.Main) {
-                textViewHello.text = presenter.getWeather()
+                textViewHello.text = viewModel.getWeather()
             }
         }
     }
--- app/src/main/java/com/example/weatherfetcher/feature/weather_screen/ui/WeatherScreenViewModel.kt
+++ app/src/main/java/com/example/weatherfetcher/feature/weather_screen/ui/WeatherScreenViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.weatherfetcher.feature.weather_screen.ui
 
+import androidx.lifecycle.ViewModel
 import com.example.weatherfetcher.feature.weather_screen.WeatherInteractor
 
-class WeatherScreenPresenter(val interactor: WeatherInteractor) {
+class WeatherScreenViewModel(val interactor: WeatherInteractor) : ViewModel() {
 
     suspend fun getWeather(): String {
         return interactor.getWeather()
